b1db10dc423f24948b7d9e10b417b37d23f1af51
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2025-10-02 13:36:11 +1000

    Adding file

---
+++ app/src/main/java/com/nafisa008/nafisa008recipe/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.nafisa008.nafisa008recipe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nafisa008.nafisa008recipe.ui.theme.Nafisa008RecipeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Nafisa008RecipeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+} 
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Nafisa008RecipeTheme {
+        Greeting("Android")
+    }
+}
